22b1cfbb6ae585d4a7739065fe8a31ab20f40433
br0wnrecluse <sblmnrd@gmail.com>
2025-04-11 06:11:40 +0800

    feat(ui): add responsive header component
    - Create Header component with responsive design and imported in page component
    - Configure webpack in next.config.js to support SVG imports
    - Install @svgr/webpack dependency
    - Add declaration.d.ts for TypeScript SVG imports
    - Include declaration file in tsconfig.json

---
+++ src/components/header.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Setting from "/public/assets/setting.svg";
+import Sound from "/public/assets/sound.svg";
+import Sun from "/public/assets/light-mode.svg";
+import Moon from "/public/assets/dark-mode.svg";
+
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center mt-10">
+      <Image
+        className="size-16 sm:size-20 md:size-24"
+        src={"./assets/vision-twenty-light-logo.svg"}
+        height={100}
+        width={100}
+        alt="vision-twenty-logo"
+      />
+
+      <div className="flex gap-x-2 md:gap-x-6">
+        <Sound className="size-8 sm:size-12" />
+        <Moon className="size-8 sm:size-12" />
+        <Setting className="size-8 sm:size-12" />
+      </div>
+    </div>
+  );
+}
