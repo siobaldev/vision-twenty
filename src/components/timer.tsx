80164833b93641eef94cb94093700c9811a6094a
br0wnrecluse <sblmnrd@gmail.com>
2025-04-11 08:05:43 +0800

    style: adjust text and icon size for different screen sizes and add space between time and control

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -3,12 +3,12 @@ import Stop from "/public/assets/stop.svg";
 export default function Timer() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="font-neopixel text-7xl lg:text-[17rem] select-none">
+      <h1 className="font-neopixel text-8xl sm:text-[8rem] md:text-[12rem] lg:text-[14rem] xl:text-[17rem] select-none">
         20:00
       </h1>
-      <div className="flex gap-x-8">
-        <Stop className="size-8 sm:size-12 md:size-16 lg:size-20" />
-        <Play className="size-8 sm:size-12 md:size-16 lg:size-20" />
+      <div className="flex gap-x-8 mt-10">
+        <Stop className="size-10 sm:size-12 md:size-16 lg:size-20" />
+        <Play className="size-10 sm:size-12 md:size-16 lg:size-20" />
       </div>
     </div>
   );
