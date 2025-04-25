f70ff50d077bc612184ce10d618a0a4456fef3ee
br0wnrecluse <sblmnrd@gmail.com>
2025-04-26 05:22:41 +0800

    feat: add logo for light and dark themes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,12 @@
-import Image from "next/image";
 import { ThemeSwitcher } from "./themeSwitcher";
 import { RiSettings3Fill } from "react-icons/ri";
 import { RiMusic2Fill } from "react-icons/ri";
+import Logo from "./logo";
 
 export default function Header() {
   return (
     <div className="absolute top-0 w-full flex justify-between items-center pt-10">
-      <Image
-        className="size-16 sm:size-20 md:size-24"
-        src={"./assets/vision-twenty-light-logo.svg"}
-        height={100}
-        width={100}
-        alt="vision-twenty-logo"
-      />
-
+      <Logo />
       <div className="flex gap-x-6 md:gap-x-10">
         <RiMusic2Fill className="size-8 sm:size-12" />
         <ThemeSwitcher />
---
+++ src/components/logo.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+
+export default function Logo() {
+  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return <div className="size-16 sm:size-20 md:size-24" />;
+
+  return (
+    <>
+      {theme === "light" ? (
+        <Image
+          className="size-16 sm:size-20 md:size-24"
+          src={"/assets/vision-twenty-light-logo.svg"}
+          height={100}
+          width={100}
+          alt="vision-twenty-logo"
+        />
+      ) : (
+        <Image
+          className="size-16 sm:size-20 md:size-24"
+          src={"/assets/vision-twenty-dark-logo.svg"}
+          height={100}
+          width={100}
+          alt="vision-twenty-logo"
+        />
+      )}
+    </>
+  );
+}
